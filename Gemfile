974d33e0bf59f94da4586deabfff0b2e49af5f43
Alexander Hammond <ajhammond123@gmail.com>
2017-01-26 23:55:43 -0500

    Fixed duplicate gems in Gemfile

--- Gemfile
+++ Gemfile
@@ -48,9 +48,9 @@ gem 'carrierwave-base64'
 
 gem 'paperclip'
 
-gem 'better_errors'
+#gem 'better_errors'
 
-gem 'binding_of_caller'
+#gem 'binding_of_caller'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module GarageSale
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-	config.web_console.whitelisted_ips = '192.168.0.0/16'
-	config.web_console.whiny_requests = false
+	#config.web_console.whitelisted_ips = '192.168.0.0/16'
+	#config.web_console.whiny_requests = false
 
   end
 end
