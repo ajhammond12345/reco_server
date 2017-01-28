4402a9fb01cfb87de44e7bfeefd1d1cd09b83e31
Alexander Hammond <ajhammond123@gmail.com>
2017-01-27 23:24:47 -0500

    Configuring filter request

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -60,14 +60,31 @@ logger.debug("price_min value=#{data_params[:price_min]}")
 logger.debug("price_max value=#{data_params[:price_max]}")
 
 		# convert to integer to avoid SQL injection issues
-		condition_min = data_params[:condition_min].to_i
-		condition_max = data_params[:condition_max].to_i
-		price_min = data_params[:price_min].to_i
-		price_max = data_params[:price_max].to_i
+		condition_min = data_params[:condition_min]
+		condition_max = data_params[:condition_max]
+		price_min = data_params[:price_min]
+		price_max = data_params[:price_max]
+logger.debug("Values sets from data_params")
+logger.debug("condition_min=#{condition_min}")
+logger.debug("condition_max=#{condition_max}")
+logger.debug("price_min=#{price_min}")
+logger.debug("price_max=#{price_max}")
 		condition_min ||= 0
 		condition_max ||= 10000
 		price_min ||= 0
-		price_max ||= 1000000000
+		price_max ||= 100000000
+logger.debug("condition_min=#{condition_min}")
+logger.debug("condition_max=#{condition_max}")
+logger.debug("price_min=#{price_min}")
+logger.debug("price_max=#{price_max}")
+		condition_min = condition_min.to_i
+		condition_max = condition_max.to_i
+		price_min = price_min.to_i
+		price_max = price_max.to_i
+logger.debug("condition_min=#{condition_min}")
+logger.debug("condition_max=#{condition_max}")
+logger.debug("price_min=#{price_min}")
+logger.debug("price_max=#{price_max}")
 
 		@items = Item.includes(:comments).where('item_price_in_cents >= ? and item_price_in_cents <= ? and item_condition >= ? and item_condition <= ?', price_min, price_max, condition_min, condition_max)
 	
